2fa9777325f30c649c06201c0625b15474a8cea9
Roy Brabson <brabson@alumni.duke.edu>
2026-02-04 14:51:41 -0500

    scores

--- scores.go
+++ scores.go
@@ -20,45 +20,21 @@ type MatchScores struct {
 }
 
 // MatchAlliance is the detailed results for a given team in a match at a given event.
+// It only contains data that is reported for all seasons, and ommits season-specific data.
 type MatchAlliance struct {
-	Alliance                string `json:"alliance"`
-	Team                    int    `json:"team"`
-	InitTeamProp1           bool   `json:"initTeamProp1"`
-	InitTeamProp2           bool   `json:"initTeamProp2"`
-	Robot1Auto              bool   `json:"robot1Auto"`
-	Robot2Auto              bool   `json:"robot2Auto"`
-	SpikeMarkPixel1         bool   `json:"spikeMarkPixel1"`
-	SpikeMarkPixel2         bool   `json:"spikeMarkPixel2"`
-	TargetBackdropPixel1    bool   `json:"targetBackdropPixel1"`
-	TargetBackdropPixel2    bool   `json:"targetBackdropPixel2"`
-	AutoBackdrop            int    `json:"autoBackdrop"`
-	AutoBackstage           int    `json:"autoBackstage"`
-	DcBackdrop              int    `json:"dcBackdrop"`
-	DcBackstage             int    `json:"dcBackstage"`
-	Mosaics                 int    `json:"mosaics"`
-	MaxSetLine              int    `json:"maxSetLine"`
-	EgRobot1                string `json:"egRobot1"`
-	EgRobot2                string `json:"egRobot2"`
-	Drone1                  int    `json:"drone1"`
-	Drone2                  int    `json:"drone2"`
-	MinorPenalties          int    `json:"minorPenalties"`
-	MajorPenalties          int    `json:"majorPenalties"`
-	AutoNavigatingPoints    int    `json:"autoNavigatingPoints"`
-	AutoRandomizationPoints int    `json:"autoRandomizationPoints"`
-	AutoBackstagePoints     int    `json:"autoBackstagePoints"`
-	AutoBackdropPoints      int    `json:"autoBackdropPoints"`
-	DcBackdropPoints        int    `json:"dcBackdropPoints"`
-	DcBackstagePoints       int    `json:"dcBackstagePoints"`
-	MosaicPoints            int    `json:"mosaicPoints"`
-	SetBonusPoints          int    `json:"setBonusPoints"`
-	EgLocationPoints        int    `json:"egLocationPoints"`
-	EgDronePoints           int    `json:"egDronePoints"`
-	AutoPoints              int    `json:"autoPoints"`
-	DcPoints                int    `json:"dcPoints"`
-	EndgamePoints           int    `json:"endgamePoints"`
-	PenaltyPointsCommitted  int    `json:"penaltyPointsCommitted"`
-	PrePenaltyTotal         int    `json:"prePenaltyTotal"`
-	TotalPoints             int    `json:"totalPoints"`
+	Alliance            string `json:"alliance"`
+	Team                int    `json:"team"`
+	Robot1Auto          bool   `json:"robot1Auto"`
+	Robot2Auto          bool   `json:"robot2Auto"`
+	Robot1Teleop        string `json:"robot1Teleop"`
+	Robot2Teleop        string `json:"robot2Teleop"`
+	AutoPoints          int    `json:"autoPoints"`
+	TeleopPoints        int    `json:"teleopPoints"`
+	FoulPointsCommitted int    `json:"foulPointsCommitted"`
+	PreFoulTotal        int    `json:"preFoulTotal"`
+	TotalPoints         int    `json:"totalPoints"`
+	MajorFouls          int    `json:"majorFouls"`
+	MinorFouls          int    `json:"minorFouls"`
 }
 
 // GetEventScores returns the results for a given event
